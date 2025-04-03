17feade7984149dfd5792fd9a06d744983ffc437
Soufiane Nouira <soufianenouira@gmail.comm>
2025-04-03 21:56:42 +0200

    responsiv

--- src/app/main-content/references/references.component.ts
+++ src/app/main-content/references/references.component.ts
@@ -1,176 +1,88 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateDirective, TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-references',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslatePipe],
   templateUrl: './references.component.html',
   styleUrl: './references.component.scss'
 })
 export class ReferencesComponent {
-  references = [
-    /* {
-      "author": "Soufiane",
-      "comment": "teammate6",
-      "display": "none",
-      "backgroundColor": "#3dcfb711"
-    }, */
-    {
-      "author": "Soufiane",
-      "comment": "teammate1",
-      "display": "flex",
-      "backgroundColor": "#3dcfb747"
-    },
-    {
-      "author": "Soufiane",
-      "comment": "teammate2",
-      "display": "none",
-      "backgroundColor": "#3dcfb711"
-    },
-    {
-      "author": "Soufiane",
-      "comment": "teammate3",
-      "display": "none",
-      "backgroundColor": "#3dcfb711"
-    },
-    {
-      "author": "Soufiane",
-      "comment": "teammate4",
-      "display": "none",
-      "backgroundColor": "#3dcfb711"
-    },
-    {
-      "author": "Soufiane",
-      "comment": "teammate5",
-      "display": "none",
-      "backgroundColor": "#3dcfb711"
-    },
-    {
-      "author": "Soufiane",
-      "comment": "teammate6",
-      "display": "none",
-      "backgroundColor": "#3dcfb711"
-    },
-    {
-      "author": "Soufiane",
-      "comment": "teammate1",
-      "display": "none",
-      "backgroundColor": "#3dcfb711"
-    }, 
-
-  ];
-
-  fillColors = ["#3DCFB6", "white", "white", "white", "white", "white",];
-  scrollBehavior = "smooth";
-  scrollLeftPosition:number = 160;
-
-  ngAfterViewInit() {
-    const referencesContainer = document.querySelector('.references');
-    if (referencesContainer) {
-      referencesContainer.scrollLeft = 0;
-      console.log(referencesContainer.scrollWidth);
-      
-      //this.fillTheRightSvg(referencesContainer.scrollLeft);
-    }
-  }
+feedbacks :{name: string; feedback: string, ref: string}[]= 
+[
+{
+  name: 'test',
+  feedback: 'doppelter test',
+  ref: 'Boss'
+},
+{
+  name: 'anderer test',
+  feedback: 'hier wird auch getestet',
+  ref: 'employee'
+},
+{
+  name: 'mustermann, max',
+  feedback: 'ewige testung',
+  ref: 'putzfrau'
+},
+{ feedback: "Great teamwork!",
+   name: "Alice",
+    ref: "Software Engineer"
+   },
+{ feedback: "Very reliable.",
+   name: "Bob",
+    ref: "Project Manager"
+   },
   
-  scrollLeft() {
-    const referencesContainer = document.querySelector('.references');
-    if (referencesContainer) {
-      if(this.scrollLeftPosition > 160){
-        this.scrollBehavior = "smooth";
-        referencesContainer.scrollLeft -= 766;
-        this.scrollLeftPosition -= 766;
-      }else{
-        this.scrollBehavior = "auto";
-        referencesContainer.scrollLeft = 3991;
-        this.scrollLeftPosition = 3991;
-      }
-      //this.fillTheRightSvg(this.scrollLeftPosition);
-    }
-  }
+];
+currentOffset = 0;
 
+middleIndex = 2; 
+
+
+slideLeft() {
+  if(this.middleIndex < this.feedbacks.length -1){
+    this.middleIndex++;
+  }else{this. middleIndex = this.middleIndex}  
+  this.shiftCards('right');
   
-  scrollRight() {
-    const referencesContainer = document.querySelector('.references');
-    if (referencesContainer) {
-      if(this.scrollLeftPosition < 3990){
-        this.scrollBehavior = "smooth";
-        referencesContainer.scrollLeft += 766;
-        this.scrollLeftPosition += 766;
-      }else{
-        this.scrollBehavior = "auto";
-        referencesContainer.scrollLeft = 160;
-        this.scrollLeftPosition = 160;
-      }
-      //this.fillTheRightSvg(this.scrollLeftPosition);
-    }
-  }
+}
 
-  clearFillColors(){
-    for (let index = 0; index < this.fillColors.length; index++) {
-      this.fillColors[index] = "white";
-    }
+slideRight() {
+  if (this.middleIndex != 0) {
+    this.middleIndex--; 
+  }else{
+    this.middleIndex = 0
   }
+  this.shiftCards('left'); 
+}
 
-  clearReferencesImg(){
-    for (let index = 0; index < this.references.length; index++) {
-      this.references[index].display = "none";
-      this.references[index].backgroundColor = "#3dcfb711";
-    }
+shiftCards(direction: string) {
+  const feedbackCards = document.querySelectorAll('.feedback-card');
+  if (direction === 'left') {
+    this.currentOffset += 100; 
+  } else if (direction === 'right') {
+    this.currentOffset -= 100; 
+  }
+  if(this.currentOffset > 200){
+    this.currentOffset = 200;
+  }if(this.currentOffset < -200){
+    this.currentOffset= -200
+  }
+  feedbackCards.forEach((card: any, index:number) => {
+    let isActive:boolean = index === this.middleIndex;
+    card.style.transform = `translateX(${this.currentOffset}%) scale(${isActive ? 1.1 : 0.8})` ;
     
-  }
+  });
+}
+getCardClass(index: number): string {
+  if (index < this.middleIndex) return 'left feedback-card';
+  if (index > this.middleIndex) return 'right feedback-card';
+  return 'feedback-card';
+}
 
-  fillTheRightSvg(position:number){
-    console.log(position);
-    switch (position) {
-      case 160: 
-      this.clearFillColors();
-      this.clearReferencesImg();
-      this.fillColors[0] = "#3DCFB6"
-      this.references[1].display = "flex";
-      this.references[1].backgroundColor = "#3dcfb747";
-      break;
-      case 926:
-        this.clearFillColors();
-        this.clearReferencesImg();
-        this.fillColors[1] = "#3DCFB6"
-        this.references[2].display = "flex";
-        this.references[2].backgroundColor = "#3dcfb747";
-        break;
-        case 1692:
-        this.clearFillColors();
-        this.clearReferencesImg();
-        this.fillColors[2] = "#3DCFB6"
-        this.references[3].display = "flex";
-        this.references[3].backgroundColor = "#3dcfb747";
-        break;
-        case 2458:
-        this.clearFillColors();
-        this.clearReferencesImg();
-        this.fillColors[3] = "#3DCFB6"
-        this.references[4].display = "flex";
-        this.references[4].backgroundColor = "#3dcfb747";
-        break;
-        case 3224:
-        this.clearFillColors();
-        this.clearReferencesImg();
-        this.fillColors[4] = "#3DCFB6"
-        this.references[5].display = "flex";
-        this.references[5].backgroundColor = "#3dcfb747";
-        break;
-        case 3990:
-        this.clearFillColors();
-        this.clearReferencesImg();
-        this.fillColors[5] = "#3DCFB6"
-        this.references[6].display = "flex";
-        this.references[6].backgroundColor = "#3dcfb747";
-        break;
-       
-      default:
-        
-    }
-  }
+
 
 }
